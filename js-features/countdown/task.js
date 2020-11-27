387f12844abff98d432c709a2453d0e09c125157
Natalya Chekhonina <pfzw90@gmail.com>
2020-11-27 19:15:33 +0300

    Added 1.1 (cowntdown clock in HH:MM:SS)

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,8 +1,10 @@
 'use strict';
 
-let t = setInterval(() => {
-    let time = document.getElementById('timer').textContent;
-    document.getElementById('timer').textContent--;
+let time = document.getElementById('timer').textContent;
+document.getElementById('timer').textContent = new Date(time * 1000).toISOString().substr(11, 8);
+let t = setInterval(() => {   
+    time--;
+    document.getElementById('timer').textContent = new Date(time * 1000).toISOString().substr(11, 8); 
     if (time == 0) {
         clearInterval(t);
         window.alert('Вы победили в конкурсе!');
@@ -12,3 +14,5 @@ let t = setInterval(() => {
         document.body.append(a);
         a.click();
     }}, 1000);
+  
+    
