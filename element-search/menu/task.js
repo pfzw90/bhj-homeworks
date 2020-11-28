1a70f7db4c5efced60a1cb2a067b174e9df369de
pfzw90 <pfzw90@gmail.com>
2020-11-28 21:48:50 +0300

    Updated with classList methods

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,17 +3,17 @@
 for (let item of Array.from(document.querySelectorAll('.menu__item .menu__link'))) {
 
     let sub_item = item.nextElementSibling;
-    if (sub_item != null && sub_item.className.includes('menu menu_sub')) {
+    if (sub_item != null && sub_item.classList.contains('menu', 'menu_sub')) {
         item.onclick = () => { 
-            if (sub_item.className.includes(' menu_active')) sub_item.className = sub_item.className.replace(' menu_active','');
-            else sub_item.className += ' menu_active'; 
+            if (sub_item.classList.contains('menu_active')) sub_item.className = sub_item.classList.remove('menu_active');
+            else sub_item.classList.add('menu_active'); 
 
             let this_menu = item.closest('.menu');
             
             for (let item_close_ul of Array.from(this_menu.children)) {
                 for (let item_close_li of Array.from(item_close_ul.children)) {
-                    if (item != item_close_li && item_close_li.className.includes('menu__link') && item_close_li.nextElementSibling != null) {
-                        item_close_li.nextElementSibling.className = 'menu menu_sub';
+                    if (item != item_close_li && item_close_li.classList.contains('menu__link') && item_close_li.nextElementSibling != null) {
+                        item_close_li.nextElementSibling.classList.remove('menu_active');
                     };
                 };
             };
