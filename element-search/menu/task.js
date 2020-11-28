28e0f612ee42a086bdaa3f601873adcb83db3b76
pfzw90 <pfzw90@gmail.com>
2020-11-28 20:45:13 +0300

    Task 2 update

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,23 @@
 "use strict";
 
-
 for (let item of Array.from(document.querySelectorAll('.menu__item .menu__link'))) {
+
     let sub_item = item.nextElementSibling;
     if (sub_item != null && sub_item.className.includes('menu menu_sub')) {
         item.onclick = () => { 
             if (sub_item.className.includes(' menu_active')) sub_item.className = sub_item.className.replace(' menu_active','');
             else sub_item.className += ' menu_active'; 
+
+            let this_menu = item.closest('.menu');
+            
+            for (let item_close_ul of Array.from(this_menu.children)) {
+                for (let item_close_li of Array.from(item_close_ul.children)) {
+                    if (item != item_close_li && item_close_li.className.includes('menu__link') && item_close_li.nextElementSibling != null) {
+                        item_close_li.nextElementSibling.className = 'menu menu_sub';
+                    };
+                };
+            };
             return false; 
-        }
-    }
-}
+        };
+    };  
+};
