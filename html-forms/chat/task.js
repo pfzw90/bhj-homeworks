51a36f23d54ef3c32bd640e39f24b97c4d8161db
Natalya Chekhonina <pfzw90@gmail.com>
2020-12-09 14:41:36 +0300

    Stop timer on blur in task 1

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -23,12 +23,13 @@ const printMessage = (clss, field) => {
 
 const setTimer = function(elem) {
     if (elem.dataset.timer) clearInterval(elem.dataset.timer);
-    elem.dataset.timer = setInterval(printMessage, 10000, '', elem)
+    elem.dataset.timer = setInterval(printMessage, 30000, '', elem)
 }
 
 document.querySelector('div.chat-widget').addEventListener('click', function() {this.classList.add('chat-widget_active')});
 
 document.getElementById('chat-widget__input').addEventListener('focus', function() {setTimer(this)});
+document.getElementById('chat-widget__input').addEventListener('blur', function() {clearInterval(this.dataset.timer)})
 
 document.getElementById('chat-widget__input').addEventListener('keypress', function(ev) {
     if (this.value && ev.code == 'Enter') {printMessage('message_client', this); printMessage('', this); setTimer(this);}
