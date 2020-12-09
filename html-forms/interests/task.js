0c3869776ccd0a00b29ddc2c953e8f4d4668ef2c
Natalya Chekhonina <pfzw90@gmail.com>
2020-12-09 16:41:33 +0300

    Task 2 still stuck with recursion

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,28 +1,11 @@
 "use strict";
 
-function switchChildren(elem) {
-    let state = elem.checked;
-    let parent  = elem.closest('li')
-    // console.log(parent.querySelector('ul.interests_active'))
-    if (parent.querySelector('ul.interests_active') !== 'null') {
-        
-        console.log('hey')
-        Array.from(parent.querySelectorAll('ul.interests_active > li > label > input')).forEach((child) => {
-            child.checked = state;
-            switchChildren(child);
-            let p1  = child.closest('li')
-            console.log(p1.querySelector('ul.interests_active'))
-        })
-
-        console.log()
-
-    }
-    else return;
-}
-
 Array.from(document.querySelectorAll('div > ul > li > label > input')).forEach((box) => {
     box.addEventListener('change', function() {
-        console.log('oppsa')
-        switchChildren(this);
+        let state = this.checked;
+        let parent = this.closest('li');
+        Array.from(parent.querySelectorAll('input.interest__check')).forEach((child) => {
+            child.checked = state;
+        })
     })
 })
