11a815743f81ff47ee480438677f0de4fd43f283
pfzw90 <pfzw90@gmail.com>
2020-12-14 21:33:49 +0300

    Задание 1 костылявое, но пойдет :laughing:

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,7 +2,7 @@
 
 
 Array.from(document.getElementsByClassName('has-tooltip')).forEach((function(link){
-    console.log(link)
+
     let tooltip = document.createElement("div");
     tooltip.innerText = link.title;
     tooltip.className = "tooltip";
@@ -17,16 +17,21 @@ Array.from(document.getElementsByClassName('has-tooltip')).forEach((function(lin
 
         tip.classList.toggle('tooltip_active');
 
-        
         if (tip.classList.contains('tooltip_active')) {
             let linkParams = this.getBoundingClientRect();
             let pos = this.dataset.position;
 
-            if (pos == 'top' || pos == 'bottom') { tip.style.setProperty('left', Math.floor(linkParams.left) + 'px'); }
-            if (pos == 'left' || pos == 'right') { tip.style.setProperty('top', Math.floor(linkParams.top) + 'px'); }
-            if (pos == 'right') {tip.style.setProperty('left' , Math.floor(linkParams.left + linkParams.width + 10) + 'px'); }
-            if (pos == 'left') {tip.style.setProperty('left' , Math.floor(linkParams.left - tip.offsetWidth - 10) + 'px'); }
-        }
+            if (pos == 'top' || pos == 'bottom') { 
+                tip.style.setProperty('left', linkParams.left + 'px');
+                if (pos == 'top') tip.style.setProperty('top', linkParams.top - tip.offsetHeight - 10 + 'px');
+                if (pos == 'bottom') tip.style.setProperty('top', linkParams.top + linkParams.height + 10 + 'px');
+             }
 
+            if (pos == 'left' || pos == 'right') { 
+                tip.style.setProperty('top', linkParams.top + 'px'); 
+                if (pos == 'right') tip.style.setProperty('left' , linkParams.left + linkParams.width + 10 + 'px'); 
+                if (pos == 'left') tip.style.setProperty('left' , linkParams.left - tip.offsetWidth - 10 + 'px'); 
+            }
+        }
     });
 }));
